d2dcf2e2ba94e919e1bbe5be38fc75c303fb212d
youssafB <bouraha.youssaf@gmail.com>
2026-01-21 20:01:37 +0000

    update

---
+++ code/bronze/config.py
@@ -0,0 +1,47 @@
+
+
+
+ 
+INGESTION_CONFIG = [
+    {
+        "source": "crm",
+        "path": "/Volumes/workspace/bronze/source_system/source_crm/cust_info.csv",
+        "table": "crm_cust_info"
+    },
+
+
+    {
+        "source": "crm",
+        "path": "/Volumes/workspace/bronze/source_system/source_crm/prd_info.csv",
+        "table": "crm_prd_info"
+    },
+
+    {
+        "source": "crm",
+        "path": "/Volumes/workspace/bronze/source_system/source_crm/sales_details.csv",
+        "table": "crm_sales_details"
+    },
+
+
+    {
+        "source": "erp",
+        "path": "/Volumes/workspace/bronze/source_system/source_erp/CUST_AZ12.csv",
+        "table": "erp_cust_az12"
+    },
+
+    {
+        "source": "erp",
+        "path": "/Volumes/workspace/bronze/source_system/source_erp/LOC_A101.csv",
+        "table": "erp_loc_a101"
+    },
+
+    {
+        "source": "erp",
+        "path": "/Volumes/workspace/bronze/source_system/source_erp/PX_CAT_G1V2.csv",
+        "table": "erp_px_cat_g1v2"
+    }
+
+
+
+
+]
